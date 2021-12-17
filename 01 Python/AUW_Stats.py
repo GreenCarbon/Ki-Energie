db422f1e27b40e9e32a5c517914be5580c161007
GreenCarbon <94566820+GreenCarbon@users.noreply.github.com>
2021-12-17 18:11:24 +0100

    2021-12-17 AUW_StatKurven angelegt und versorgt

--- 01 Python/AUW_Stats.py
+++ 01 Python/AUW_Stats.py
@@ -31,8 +31,23 @@ sql_datum_von = "2021-11-14 00:00:00"
 sql_datum_bis = "2021-11-16 00:00:00"
 
 # Schreiben eines statistischen Records
-def write_stat_rec(Id1, Id2, From_Datum, From_Zeit, To_Datum, To_Zeit, Start_val, Ende_val):
-      print("Start: ", Id1, " | Ende: ", Id2, " | Von: ", From_Datum, " " , To_Zeit, " | Temperatur von : ", Start_val , " bis : " , Ende_val)
+def write_stat_rec(Id1, Id2, From_Datum, From_Time, To_Datum, To_Time, Start_val, Ende_val):
+      print("Start: ", Id1, " | Ende: ", Id2, " | Von: ", From_Time, " " , To_Time, " | Temperatur von : ", Start_val , " bis : " , Ende_val)
+      diff = Ende_val - Start_val
+      phase = "Phase"
+      cursor = con.cursor()
+      try:
+            sql_anweisung = """INSERT INTO AUW_StatKurven (id_Messwert_1, id_Messwert_2, start_time, end_time, wert_num_1, wert_num_2, wert_diff, phase)
+                               VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"""
+                               
+            sql_werte = (Id1, Id2, From_Time, To_Time, Start_val, Ende_val, diff, phase)
+
+            db_ergebnis = cursor.execute(sql_anweisung, sql_werte)
+            con.commit()
+            
+      except mysql.connector.Error as error:
+            print("Fehler beim Schreiben in die AUW_StatKurven: {}".format(error))
+
       return
 
 ############ Einstieg Hauptprogramm
@@ -72,11 +87,11 @@ sel1_stat1 = []     #np.empty([0,4])  #ID, Datum, Zeit, Temperatur
 for mwd in res_data:
       if ([mwd[2]] == aktval):
             continue        # gleiche Werte überspringen
-      sel1_id.append(getVal([mwd[0]]))
-      sel1_datum.append(getVal([mwd[1]])) 
-      sel1_zeit.append(getVal([mwd[1]]))
-      sel1_wert.append(getVal([mwd[2]]))
-      aktval = getVal([mwd[2]])
+      sel1_id.append(getVal(mwd[0]))
+      sel1_datum.append(getVal(mwd[1])) 
+      sel1_zeit.append(getVal(mwd[1]))
+      sel1_wert.append(getVal(mwd[2]))
+      aktval = getVal(mwd[2])
 #print(sel1_id)
 
 # Am Ende im Array folgende Werte ermitten
@@ -109,16 +124,15 @@ if (len(sel1_id) > 1):
             going_up = False
             
             
-# Duchgehen der Werte ab der zweiten Zeile (i=1)
+# Durchgehen der Werte ab der zweiten Zeile (i=1)
 i = 0     
-print("Gesamtanzahl Werte = ", len(sel1_id))       
+
 while i < len(sel1_id) :
       i+=1
       
       if ((i + 2) == len(sel1_id)):      # Letzte zeile erreicht   
             write_stat_rec(from_Id, sel1_id[i], from_datum, from_zeit, sel1_datum[i], sel1_zeit[i], start_value, sel1_wert[i])    
             break    
-      print("Werte: ", sel1_wert[i-1], " / " , sel1_wert[i])
      
       if (going_up and sel1_wert[i] > sel1_wert[i-1]):  # Weiter steigend
             high_value = sel1_wert[i]
@@ -151,7 +165,6 @@ while i < len(sel1_id) :
             continue
             
       
-
 # Aus den Phasen dann am Ende ermitteln
 #     + Höchste Temperatur insgesamt ID, Wert und Zeit)
 #     + niedrigste Temperatur insgesamt
