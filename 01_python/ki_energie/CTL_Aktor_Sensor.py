55e8f321040dc5365be1037222e1fb5d16dfe03a
GreenCarbon <94566820+GreenCarbon@users.noreply.github.com>
2022-05-12 20:56:26 +0200

    DB Erweiterung Geraete, Variable Zuordnung Sensoren

--- 01_python/ki_energie/CTL_Aktor_Sensor.py
+++ 01_python/ki_energie/CTL_Aktor_Sensor.py
@@ -3,6 +3,7 @@
 # externe Kommunikation mit Sensoren und Aktoren festgelegt
 # -----------------------------------------------------------
 ###
+from concurrent.futures.process import _chain_from_iterable_of_lists
 from time import strptime
 import mysql.connector
 import matplotlib.pyplot as plt
@@ -16,9 +17,9 @@ from datetime import *
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-#from PIL import Image, ImageTk
-#from requests.auth import HTTPDigestAuth
-#import requests
+from PIL import Image, ImageTk
+from requests.auth import HTTPDigestAuth
+import requests
 import xml.etree.ElementTree as xml
 # Django Framework
 from django.db import models
@@ -49,52 +50,10 @@ class External_Interface() :
         
         
         
-        
-class Temperatur_Sensor():
-    __Hardware_Typ = "LOXONE"
-    __Sensor_Typ = "STELLANTRIEB 10V"
-    __IP_Addr = "192.168.20.201"
-    __Port = "2710"
-    __Senor_Id = 0
-    
-    def __init__(self, id):
-# Der Sensor ermittelt aus der Datenbank seine Betriebs- und Kommunikationswerte   
-        self.hardware_Typ = "LOXONE"
-        self.Sensor_Typ = "STELLANTRIEB 10V"
-        self.IP_Addr = "192.168.20.201"
-        self.Port = "2710"
-        self.senor_Id = id     
-        pass
-    
-    def getCurrentTemperatur(self):
-        r = requests.get("http://pi001/addons/xmlapi/state.cgi?datapoint_id=1417") #TS001 auf Schreibtisch Arbeitszimmer
-        rtn = r.status_code
-        hdr = r.headers
-        value = r.text
-        xmldoc = xml.fromstring(value)
 
-        temp1 = float(xmldoc.find('./datapoint').attrib['value'].replace(",", "."))
-        #print("Temperatur = %f", temp1)
-        return temp1;
-
-    def getTargetTemperatur(self):
-        r = requests.get("http://pi001/addons/xmlapi/state.cgi?datapoint_id=1467") #TA0001 von Wohnhimmer
-        rtn = r.status_code
-        hdr = r.headers
-        value = r.text
-        xmldoc = xml.fromstring(value)
-
-        temp1 = float(xmldoc.find('./datapoint').attrib['value'].replace(",", "."))
-        #print("Temperatur = %f", temp1)
-        return temp1;
-    
-    def getCurrentVentil(self):
-        ventil = 50
-        return ventil
-    
-    def isSensorActive(self):
-        return True
-    
+#*********************************************************************************
+# Aktor zur Regelung / Bestimmung der Temperaruren
+#*********************************************************************************
 class Temperatur_Aktor():   
     def __init__(self, id, name):
         self.name = name
@@ -103,7 +62,7 @@ class Temperatur_Aktor():
         self.logger = initLogger('root')
         
 # Alle Sensoren zu diesem Aktor lesen und instantiieren  
-        ctl_list = []      
+        self.ctl_list = []      
         try:
             #sql_anweisung = """SELECT * FROM ki_rg_actor_sensor 
             #    WHERE isActive = true """
@@ -118,9 +77,9 @@ class Temperatur_Aktor():
                 sensor = KiRgSensor.objects.filter(id = Id).first()
                 sensname = getattr(sensor, "name")
                 typ = getattr(sensor, "typen_id")
-                logging.info("Sensor " + sensname + " hinzugefügt.")
+                logging.info("Sensor " + sensname + " Typ: " + str(typ) + " hinzugefügt.")
                 if (typ == 1): # Vorerst nur Temperatursensoren berücksichtigen
-                    ctl_list.append(Temperatur_Sensor(Id))
+                    self.ctl_list.append(Temperatur_Sensor(Id))
 # Aktor-Controller als Hintergrundprozess starten                
             thread = threading.Thread(target=self.run, args=())
             thread.daemon = True
@@ -129,13 +88,13 @@ class Temperatur_Aktor():
             logging.error("Fehler beim Starten des Actor-Controllers aufgetreten: {}".format(error))
     
     def run(self):
-        Sensor1 = Temperatur_Sensor()
-        while Sensor1.getTargetTemperatur() > 15:
-            print("Aktuelle   Temperatur: ", Sensor1.getCurrentTemperatur())
-            print("Gewünschte Temperatur: ", Sensor1.getTargetTemperatur())
-            TIME.sleep(1)
+        while 2 > 1:
+            for sensor in  self.ctl_list:
+                print("Aktuelle   Temperatur: ", sensor.getGeraeteArt(), " / ",  sensor.getCurrentTemperatur())
+                #print("Gewünschte Temperatur: ", ctl.getTargetTemperatur())
+            TIME.sleep(5)
     
- # Am Ausgang für den Aktor die gewünschte Solltemperatur einstellen   
+# Am Ausgang für den Aktor die gewünschte Solltemperatur einstellen   
     def set_Ausgang_Temp(self, p_Temperatur):
         response = requests.get('http://192.168.20.201:2710/data/status', auth=HTTPDigestAuth('Karsten', 'Only4#Winners'))
         print(response)
@@ -148,7 +107,245 @@ class Temperatur_Aktor():
         
     def isAktorActive(self):
         return True    
+ 
+#*********************************************************************************
+# Aktor zur Regelung / Bestimmung der Luftfeuchte
+#*********************************************************************************
+   
+class Feuchte_Aktor():   
+    def __init__(self, id, name):
+        self.name = name
+        self.Aktor_Id = id
+        self.con = db_conn()
+        self.logger = initLogger('root')
+        
+# Alle Sensoren zu diesem Aktor lesen und instantiieren  
+        self.ctl_list = []      
+        try:
+            #sql_anweisung = """SELECT * FROM ki_rg_actor_sensor 
+            #    WHERE isActive = true """
+            sensors = KiRgActorSensor.objects.filter(isactive = 1).filter(aktor_id= self.Aktor_Id)
+            #cursor = self.con.cursor()
+            #cursor.execute(sql_anweisung)
+            #sql_erg = cursor.fetchall()
+            sensname = ''
+            #cursor.close()
+            for ctl in sensors:
+                Id = getattr(ctl, "id")  
+                sensor = KiRgSensor.objects.filter(id = Id).first()
+                sensname = getattr(sensor, "name")
+                typ = getattr(sensor, "typen_id")
+                logging.info("Sensor " + sensname + " Typ: " + str(typ) + " hinzugefügt.")
+                if (typ == 1): # Vorerst nur Temperatursensoren berücksichtigen
+                    self.ctl_list.append(Temperatur_Sensor(Id))
+# Aktor-Controller als Hintergrundprozess starten                
+            thread = threading.Thread(target=self.run, args=())
+            thread.daemon = True
+            thread.start()    
+        except mysql.connector.Error as error:
+            logging.error("Fehler beim Starten des Actor-Controllers aufgetreten: {}".format(error))
     
+    def run(self):
+        while 2 > 1:
+            for ctl in  self.ctl_list:
+                print("Aktuelle   Temperatur: ", ctl.getCurrentTemperatur())
+                print("Gewünschte Temperatur: ", ctl.getTargetTemperatur())
+                TIME.sleep(5)
+    
+# Am Ausgang für den Aktor die gewünschte Solltemperatur einstellen   
+    def set_Ausgang_Temp(self, p_Temperatur):
+        response = requests.get('http://192.168.20.201:2710/data/status', auth=HTTPDigestAuth('Karsten', 'Only4#Winners'))
+        print(response)
+        pass
+    
+ # Am Ausgang für den Aktor die Ventilstellung in % einstellen. 
+ # 0 = geschlossen, 100 / komplett offen   
+    def set_Ausgang_Ventil(self, p_Prozent):
+        pass
+        
+    def isAktorActive(self):
+        return True        
+   
+   
+#*********************************************************************************
+# Aktor zur Regelung / Bestimmung der Helligkeit
+#*********************************************************************************
+   
+class Helligkeit_Aktor():   
+    def __init__(self, id, name):
+        self.name = name
+        self.Aktor_Id = id
+        self.con = db_conn()
+        self.logger = initLogger('root')
+        
+# Alle Sensoren zu diesem Aktor lesen und instantiieren  
+        self.ctl_list = []      
+        try:
+            #sql_anweisung = """SELECT * FROM ki_rg_actor_sensor 
+            #    WHERE isActive = true """
+            sensors = KiRgActorSensor.objects.filter(isactive = 1).filter(aktor_id= self.Aktor_Id)
+            #cursor = self.con.cursor()
+            #cursor.execute(sql_anweisung)
+            #sql_erg = cursor.fetchall()
+            sensname = ''
+            #cursor.close()
+            for ctl in sensors:
+                Id = getattr(ctl, "id")  
+                sensor = KiRgSensor.objects.filter(id = Id).first()
+                sensname = getattr(sensor, "name")
+                typ = getattr(sensor, "typen_id")
+                logging.info("Sensor " + sensname + " Typ: " + str(typ) + " hinzugefügt.")
+                if (typ == 1): # Vorerst nur Temperatursensoren berücksichtigen
+                    self.ctl_list.append(Temperatur_Sensor(Id))
+# Aktor-Controller als Hintergrundprozess starten                
+            thread = threading.Thread(target=self.run, args=())
+            thread.daemon = True
+            thread.start()    
+        except mysql.connector.Error as error:
+            logging.error("Fehler beim Starten des Actor-Controllers aufgetreten: {}".format(error))
+    
+    def run(self):
+        while 2 > 1:
+            for ctl in  self.ctl_list:
+                print("Aktuelle   Temperatur: ", ctl.getCurrentTemperatur())
+                print("Gewünschte Temperatur: ", ctl.getTargetTemperatur())
+                TIME.sleep(5)
+    
+# Am Ausgang für den Aktor die gewünschte Solltemperatur einstellen   
+    def set_Ausgang_Temp(self, p_Temperatur):
+        response = requests.get('http://192.168.20.201:2710/data/status', auth=HTTPDigestAuth('Karsten', 'Only4#Winners'))
+        print(response)
+        pass
+    
+ # Am Ausgang für den Aktor die Ventilstellung in % einstellen. 
+ # 0 = geschlossen, 100 / komplett offen   
+    def set_Ausgang_Ventil(self, p_Prozent):
+        pass
+        
+    def isAktorActive(self):
+        return True          
+    
+#*********************************************************************************
+# Aktor zur Regelung / Bestimmung der Leistung
+#*********************************************************************************
+   
+class Leistung_Aktor():   
+    def __init__(self, id, name):
+        self.name = name
+        self.Aktor_Id = id
+        self.con = db_conn()
+        self.logger = initLogger('root')
+        
+# Alle Sensoren zu diesem Aktor lesen und instantiieren  
+        self.ctl_list = []      
+        try:
+            #sql_anweisung = """SELECT * FROM ki_rg_actor_sensor 
+            #    WHERE isActive = true """
+            sensors = KiRgActorSensor.objects.filter(isactive = 1).filter(aktor_id= self.Aktor_Id)
+            #cursor = self.con.cursor()
+            #cursor.execute(sql_anweisung)
+            #sql_erg = cursor.fetchall()
+            sensname = ''
+            #cursor.close()
+            for ctl in sensors:
+                Id = getattr(ctl, "id")  
+                sensor = KiRgSensor.objects.filter(id = Id).first()
+                sensname = getattr(sensor, "name")
+                typ = getattr(sensor, "typen_id")
+                logging.info("Sensor " + sensname + " Typ: " + str(typ) + " hinzugefügt.")
+                if (typ == 1): # Vorerst nur Temperatursensoren berücksichtigen
+                    self.ctl_list.append(Temperatur_Sensor(Id))
+# Aktor-Controller als Hintergrundprozess starten                
+            thread = threading.Thread(target=self.run, args=())
+            thread.daemon = True
+            thread.start()    
+        except mysql.connector.Error as error:
+            logging.error("Fehler beim Starten des Actor-Controllers aufgetreten: {}".format(error))
+    
+    def run(self):
+        while 2 > 1:
+            for ctl in  self.ctl_list:
+                print("Aktuelle   Temperatur: ", ctl.getCurrentTemperatur())
+                #print("Gewünschte Temperatur: ", ctl.getTargetTemperatur())
+            TIME.sleep(5)
+    
+# Am Ausgang für den Aktor die gewünschte Solltemperatur einstellen   
+    def set_Ausgang_Temp(self, p_Temperatur):
+        response = requests.get('http://192.168.20.201:2710/data/status', auth=HTTPDigestAuth('Karsten', 'Only4#Winners'))
+        print(response)
+        pass
+    
+ # Am Ausgang für den Aktor die Ventilstellung in % einstellen. 
+ # 0 = geschlossen, 100 / komplett offen   
+    def set_Ausgang_Ventil(self, p_Prozent):
+        pass
+        
+    def isAktorActive(self):
+        return True       
+    
+    
+#*********************************************************************************
+# Sensor zur Temperaturbestimmung
+#*********************************************************************************
+        
+class Temperatur_Sensor():
+    __Hardware_Typ = "LOXONE"
+    __Sensor_Typ = "STELLANTRIEB 10V"
+    __IP_Addr = "192.168.20.201"
+    __Port = "2710"
+    __Sensor_Id = 0
+    __Geraete_Art = ""
+    
+    def __init__(self, id):
+# Der Sensor ermittelt aus der Datenbank seine Betriebs- und Kommunikationswerte   
+        #self.hardware_Hersteller = "LOXONE"
+        #self.Sensor_Typ = "STELLANTRIEB 10V"
+        #self.IP_Addr = "192.168.20.201"
+        #self.Port = "2710"
+        #self.Sensor_Id = id     
+        sensor = KiRgSensor.objects.filter(id = id).first()
+        self.g_id = getattr(sensor, "geraete_id")
+        geraet = Geraete.objects.filter(geraete_id = getattr(sensor, "geraete_id")).first()
+        self.hardware_Hersteller = getattr(geraet, "hw_hersteller")
+        self.geraete_Art = getattr(geraet, "geraete_art")
+        self.Sensor_Typ = getattr(geraet, "geraete_art")
+        self.IP_Addr = getattr(geraet, "ip_address")
+        self.Port = getattr(geraet, "ip_port")
+        self.get_request = getattr(geraet, "get_request")
+        self.senor_Id = id
+        pass
+    
+    def getCurrentTemperatur(self):
+        r = requests.get(self.get_request)
+        rtn = r.status_code
+        hdr = r.headers
+        value = r.text
+        xmldoc = xml.fromstring(value)
+
+        temp1 = float(xmldoc.find('./datapoint').attrib['value'].replace(",", "."))
+        #print("Temperatur = %f", temp1)
+        return temp1;
+    
+    def getGeraeteArt(self):
+        return self.geraete_Art;
+    
+    #def getTargetTemperatur(self):
+    #    r = requests.get("http://pi001/addons/xmlapi/state.cgi?datapoint_id=1467") #TA0001 von Wohnhimmer
+    #    rtn = r.status_code
+    #    hdr = r.headers
+    #    value = r.text
+    #    xmldoc = xml.fromstring(value)
+    #
+    #        temp1 = float(xmldoc.find('./datapoint').attrib['value'].replace(",", "."))
+    #        #print("Temperatur = %f", temp1)
+    #        return temp1;
+    
+    def getCurrentVentil(self):
+        ventil = 50
+        return ventil
+    
+    def isSensorActive(self):
+        return True         
  # START <<<<<<<<
  
 #Sensor1 = Temperatur_Sensor()
