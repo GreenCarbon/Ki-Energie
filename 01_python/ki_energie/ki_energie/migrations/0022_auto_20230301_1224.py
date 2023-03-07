deee43ea9cb61ff91ee1082670d7e3ec5899837f
karsten ubuntu <karsten.schneider@posteo.de>
2023-03-07 08:52:10 +0100

    Test Commit mit Ubuntu

--- 01_python/ki_energie/ki_energie/migrations/0022_auto_20230301_1224.py
+++ 01_python/ki_energie/ki_energie/migrations/0022_auto_20230301_1224.py
@@ -26,49 +26,5 @@ class Migration(migrations.Migration):
                 ('log_user', models.CharField(blank=True, max_length=20, null=True)),
             ],
         ),
-        migrations.RenameField(
-            model_name='geraete',
-            old_name='geraete_id',
-            new_name='id',
-        ),
-        # migrations.RemoveField(
-        #     model_name='raumliste',
-        #     name='raum_id',
-        # ),
-        migrations.AddField(
-            model_name='geraete',
-            name='connection',
-            field=models.CharField(blank=True, max_length=200, null=True),
-        ),
-        migrations.AddField(
-            model_name='geraete',
-            name='hw_hersteller',
-            field=models.CharField(blank=True, max_length=50, null=True),
-        ),
-        migrations.AddField(
-            model_name='geraete',
-            name='ip_address',
-            field=models.CharField(blank=True, max_length=30, null=True),
-        ),
-        migrations.AddField(
-            model_name='geraete',
-            name='ip_port',
-            field=models.CharField(blank=True, max_length=5, null=True),
-        ),
-        migrations.AddField(
-            model_name='geraete',
-            name='put_request',
-            field=models.CharField(blank=True, max_length=300, null=True),
-        ),
-        migrations.AddField(
-            model_name='raumliste',
-            name='id',
-            field=models.AutoField(
-                db_column='Id', default=0, primary_key=True, serialize=False),
-        ),
-        migrations.AddField(
-            model_name='sensorvaluetypes',
-            name='comment',
-            field=models.TextField(blank=True, null=True),
-        ),
+        
     ]
